2bbe8a7dd81e1b1cc5d0cec907e30230674f5dd3
Masahiro Sakai <masahiro.sakai@gmail.com>
2021-02-14 01:30:38 +0000

    toysolver: add 0.7.0 bottle.

--- Formula/toysolver.rb
+++ Formula/toysolver.rb
@@ -6,6 +6,12 @@ class Toysolver < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/msakai/toysolver.git"
 
+  bottle do
+    root_url "https://github.com/msakai/homebrew-tap/releases/download/toysolver-0.7.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "60b4da72b8ed82d34699dccb80de5f44c522b7d93033ec85dc6d3545419fc188"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "af8476df6088e75bdf1f10a4cb92fbb8fe859a9057936c6ab2f218cca87c48f7"
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
 
